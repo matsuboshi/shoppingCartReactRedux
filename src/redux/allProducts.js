c6ba27af1818ab0dc2fafa2535fcd3ea099b865c
matsuboshi <matsuboshi@hotmail.com>
2021-01-27 05:25:48 -0300

    Project finished

---
+++ src/redux/allProducts.js
@@ -0,0 +1,44 @@
+const allProducts = [
+  {
+    id: 1,
+    name: 'iPhone 12 MINI',
+    price: 700,
+  },
+  {
+    id: 2,
+    name: 'iPhone 12',
+    price: 800,
+  },
+  {
+    id: 3,
+    name: 'iPhone 12 PRO',
+    price: 1000,
+  },
+  {
+    id: 4,
+    name: 'iPhone 12 PRO MAX',
+    price: 1100,
+  },
+  {
+    id: 5,
+    name: 'iPhone SE',
+    price: 400,
+  },
+  {
+    id: 6,
+    name: 'iPhone 11',
+    price: 600,
+  },
+  {
+    id: 7,
+    name: 'AirPods',
+    price: 200,
+  },
+  {
+    id: 8,
+    name: 'AirPods PRO',
+    price: 250,
+  },
+];
+
+export default allProducts;
